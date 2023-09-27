cc0c2ae550d0a6121f31a187448d147ec16469cc
ict154 <radengozal@gmail.com>
2023-09-27 14:05:35 +0700

    add tawk to

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -815,6 +815,21 @@
 ╚═╝░░╚═╝░░░░░╚═╝╚═════╝░╚═════╝░╚═╝░░╚══╝  ░╚═════╝░░╚════╝░╚══════╝░░░░░╚═╝╚══════╝╚══════╝
 `);
 </script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/6088c8ac62662a09efc2ca9a/1f4b4vp9n';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <!-- END Page Settings -->
 <!-- base vendor bundle: 
 			 DOC: if you remove pace.js from core please note on Internet Explorer some CSS animations may execute before a page is fully loaded, resulting 'jump' animations 
